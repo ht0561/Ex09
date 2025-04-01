db8a218d4b82477931a732ed80c9c829b4de3358
hanna <ht0561@bs.amlnet.k12.il>
2025-04-01 17:27:18 +0300

    final pro

--- app/src/main/java/com/example/ex09/MainActivity.java
+++ app/src/main/java/com/example/ex09/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.ex09;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,13 +16,18 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     WebView wV1;
     String stringUrl;
+    TextView tv1;
 
 
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         wV1= findViewById(R.id.wV1);
+        tv1= findViewById(R.id.tV1);
         wV1.setWebViewClient(new MyWebViewClient());
         stringUrl="http://www.walla.co.il/";
 
@@ -29,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void go(View view) {
         wV1.loadUrl(stringUrl);
+        tv1.setText (stringUrl);
+
+
 
     }
 
